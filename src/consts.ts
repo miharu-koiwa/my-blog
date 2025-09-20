14ba8ae851c95eec98dbafec41a6de0ed5dc23c7
Miharu Koiwa <miko.18.ws@gmail.com>
2025-09-20 20:45:46 +0900

    Header, Footer コンポーネントの調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Miharu Koiwa's Portfolio";
+export const SITE_DESCRIPTION = '小岩 美晴のポートフォリオサイトです。';
