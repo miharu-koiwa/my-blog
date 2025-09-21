2463726399e38ac9ded3e8ef6c4d9e1be49cdc32
Miharu Koiwa <miko.18.ws@gmail.com>
2025-09-21 14:49:32 +0900

    Layout.astroの作成とトップページのレイアウト化

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Miharu Koiwa's Portfolio";
-export const SITE_DESCRIPTION = '小岩 美晴のポートフォリオサイトです。';
+export const SITE_TITLE = "Miharu Koiwa's Portfolio"
+export const SITE_DESCRIPTION = '小岩 美晴のポートフォリオサイトです。'
+export const JOB_TITLE = 'Web Developer & DX Designer'
